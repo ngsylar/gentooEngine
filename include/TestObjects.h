de5131d7323e2f90da693071cec4d91e8879af79
ngsylar <ngsylar@gmail.com>
2022-11-25 10:42:16 -0300

    cleaning jump

--- include/TestObjects.h
+++ include/TestObjects.h
@@ -1,6 +1,7 @@
 #ifndef TEST_OBJECTS_H
 #define TEST_OBJECTS_H
 
+#include "InputManager.h"
 #include "Component.h"
 
 class Ball: public Component {
@@ -10,9 +11,11 @@ class Ball: public Component {
         bool isJumping;
 
     public:
-        Ball (GameObject& associated);
-        void Start ();
-        void Update (float dt);
+        Ball(GameObject& associated);
+        void Start();
+        void Update(float dt);
+        void StartJump(float dt);
+        void HandleJump(bool isKeyDown, float dt);
 };
 
 #endif
--- src/TestObjects.cpp
+++ src/TestObjects.cpp
@@ -16,47 +16,52 @@ void Ball::Start () {
 void Ball::Update (float dt) {
     InputManager& input = InputManager::GetInstance();
 
-    if (isJumping) {
-        // if the button continues to be pressed increases the vertical jump height
-        if ((jumpHeight < jumpHeightMax) and input.IsKeyDown(KEY_ARROW_UP)) {
-            float jumpDisplacement = jumpForce * dt;
-            rigidBody->Translate(Vec2(0,-jumpDisplacement));
-            jumpHeight += jumpDisplacement;
-            if (jumpHeight > jumpHeightMax)
-                jumpHeight = jumpHeightMax;
-        }
-        // applies a constant force that gradually decreases due to the gravity force
-        else {
-            rigidBody->AddForce(Vec2(0,-((jumpForce*jumpHeight)/jumpHeightMax)));
-            rigidBody->gravityEnabled = true;
-            jumpHeight = 0.0f;
-            isJumping = false;
-        }
-        // if it hits the ceiling cancels the jump or the force applied in previous conditions
-        if (rigidBody->IsColliding(RigidBody::UP)) {
-            rigidBody->CancelForces(RigidBody::VERTICAL);
-            rigidBody->gravityEnabled = true;
-            jumpHeight = 0.0f;
-            isJumping = false;
-        }
-    }
-    if (input.KeyPress(KEY_ARROW_UP) and rigidBody->IsGrounded()) {
-        float jumpDisplacement = jumpForce * dt;
-        rigidBody->Translate(Vec2(0,-jumpDisplacement));
-        jumpHeight += jumpDisplacement;
-        rigidBody->gravityEnabled = false;
-        isJumping = true;
-    }
-    if (input.IsKeyDown(KEY_ARROW_LEFT)) {
+    if (isJumping)
+        HandleJump(input.IsKeyDown(KEY_ARROW_UP), dt);
+
+    if (input.KeyPress(KEY_ARROW_UP) and rigidBody->IsGrounded())
+        StartJump(dt);
+    if (input.IsKeyDown(KEY_ARROW_LEFT))
         rigidBody->Translate(Vec2(-runSpeed,0)*dt);
-    }
-    if (input.IsKeyDown(KEY_ARROW_RIGHT)) {
+    if (input.IsKeyDown(KEY_ARROW_RIGHT))
         rigidBody->Translate(Vec2(runSpeed,0)*dt);
-    }
 
     // // editar: remover
     // if (input.IsKeyDown(KEY_ARROW_UP))
     //     rigidBody->Translate(Vec2(0,-150)*dt);
     // if (input.IsKeyDown(KEY_ARROW_DOWN))
     //     rigidBody->Translate(Vec2(0,150)*dt);
+}
+
+void Ball::StartJump (float dt) {
+    float jumpDisplacement = jumpForce * dt;
+    rigidBody->Translate(Vec2(0,-jumpDisplacement));
+    jumpHeight += jumpDisplacement;
+    rigidBody->gravityEnabled = false;
+    isJumping = true;
+}
+
+void Ball::HandleJump (bool isKeyDown, float dt) {
+    // if the button continues to be pressed increases the vertical jump height
+    if (isKeyDown and (jumpHeight < jumpHeightMax)) {
+        float jumpDisplacement = jumpForce * dt;
+        rigidBody->Translate(Vec2(0,-jumpDisplacement));
+        jumpHeight += jumpDisplacement;
+        if (jumpHeight > jumpHeightMax)
+            jumpHeight = jumpHeightMax;
+    }
+    // applies a constant force that gradually decreases due to the gravity force
+    else {
+        rigidBody->AddForce(Vec2(0,-((jumpForce*jumpHeight)/jumpHeightMax)));
+        rigidBody->gravityEnabled = true;
+        jumpHeight = 0.0f;
+        isJumping = false;
+    }
+    // if it hits the ceiling cancels the jump or the force applied in previous conditions
+    if (rigidBody->IsColliding(RigidBody::UP)) {
+        rigidBody->CancelForces(RigidBody::VERTICAL);
+        rigidBody->gravityEnabled = true;
+        jumpHeight = 0.0f;
+        isJumping = false;
+    }
 }
